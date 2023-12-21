57db6a996a4c803bb5fb8af437b2436c7790ed9e
meerkat.log <77188221+meerkat-log@users.noreply.github.com>
2023-12-22 00:33:24 +0900

    Upload Files

--- PrefixSum/Baekjoon/11660.py
+++ PrefixSum/Baekjoon/11660.py
@@ -1,50 +1,5 @@
 # 구간 합 구하기 5
 # https://www.acmicpc.net/problem/11660
-"""
-n, m = map(int, input().split())
-
-arr = [[0 for j in range(n+1)] for i in range(n+1)]
-for i in range(1, n+1):
-    num_list = list(map(int, input().split()))
-    for j in range(1, n+1):
-        arr[i][j] = num_list[j-1]
-
-result = [[0 for j in range(n+1)] for i in range(n+1)]
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        result[i][j] = result[i][j-1] + arr[i][j]
-
-answer = [0] * m
-for i in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
-    for j in range(x1, x2+1):
-        answer[i] = answer[i] + (result[j][y2] - result[j][y1-1])
-
-for i in answer:
-    print(i)
-
-# 구간 합 구하기 5
-# https://www.acmicpc.net/problem/11660
-
-n, m = map(int, input().split())
-
-result = [[0 for j in range(n+1)] for i in range(n+1)]
-for i in range(1, n+1):
-    num_list = list(map(int, input().split()))
-    for j in range(1, n+1):
-        result[i][j] = result[i][j - 1] + num_list[j-1]
-
-answer = [0] * m
-for i in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
-    for j in range(x1, x2+1):
-        answer[i] = answer[i] + (result[j][y2] - result[j][y1-1])
-
-for i in answer:
-    print(i)
-"""
-# 구간 합 구하기 5
-# https://www.acmicpc.net/problem/11660
 from sys import stdin
 
 # N: 표의 크기, M: 합을 구해야 하는 횟수
